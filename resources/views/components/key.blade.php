6ef44cce1daba4e6c660361270486a742ef8f2d0
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2024-01-07 18:14:19 -0300

    box and key fixed

--- resources/views/components/key.blade.php
+++ resources/views/components/key.blade.php
@@ -16,7 +16,7 @@
 @endphp
 
 <button wire:click="verifyLetter('{{ $value }}')" wire:loading.attr="disabled"
-    class="flex items-center justify-center xl:w-20 xl:h-20 p-4 text-xl text-center uppercase border text-white w-[37.5px] h-[37.5px]  select-none
+    class="flex items-center justify-center 2xl:w-20 2xl:h-20 p-4 text-xl text-center uppercase border text-white w-[37.5px] h-[37.5px]  select-none
     @if ($error) bg-[#FF3C3C]  @elseif ($correct) bg-[#02e659a5]  @else bg-black @endif border-[#181818] disabled:cursor-not-allowed"
     @if ($error || $correct) disabled @endif>
     {{ $value }}
