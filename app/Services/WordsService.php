c61277c961adaf4d92c140397258a79cd5e9bcb5
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2024-01-08 17:58:26 -0300

    explode wordsArr

--- app/Services/WordsService.php
+++ app/Services/WordsService.php
@@ -4,11 +4,15 @@ namespace App\Services;
 
 class WordsService
 {
-    public static function word($num) 
+    public static function word($num)
     {
         $content = file_get_contents("files\\{$num}.txt");
 
         $words = explode(PHP_EOL, $content);
+
+        if (count($words) == 1) {
+            $words = explode("\n", $content);
+        }
 
         return $words[date('z') % count($words)];
     }
