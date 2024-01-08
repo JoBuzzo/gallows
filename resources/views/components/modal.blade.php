f0522d3ce900f3ec7aba120d5cf7f722e37f9479
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2024-01-08 13:24:40 -0300

    modal

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -9,8 +9,7 @@
 ])
 
 <div class="absolute left-0 right-0 flex items-center justify-center p-2 md:top-16">
-    <div
-        class="flex flex-col items-center justify-center w-4/5 p-8 bg-black rounded-lg md:w-1/2 md:justify-normal md:items-stretch">
+    <div class="flex flex-col items-center justify-center w-4/5 p-8 bg-black rounded-lg md:w-1/2 md:justify-normal md:items-stretch">
 
         <div class="flex items-center justify-start">
             @if ($win)
@@ -20,9 +19,9 @@
             @endif
         </div>
 
-        <div class="flex flex-col items-center justify-start mt-10 mb-5">
-            <h1 class="text-4xl">A palavra é:</h1>
-            <div class="flex flex-col items-center justify-start md:text-4xl font-extrabold uppercase @if ($win) text-[#02e659a5] @else text-[#FF3C3C] @endif">
+        <div class="flex flex-col items-center justify-center mt-10 mb-5 xl:justify-start xl:items-start">
+            <h1 class="mb-4 text-4xl font-semibold">A palavra é:</h1>
+            <div class="grid grid-cols-2 gap-6 text-2xl xl:text-4xl font-extrabold uppercase @if ($win) text-[#02e659a5] @else text-[#FF3C3C] @endif">
                 <h1>{{ $word1 }}</h1>
                 <h1>{{ $word2 }}</h1>
                 <h1>{{ $word3 }}</h1>
@@ -64,10 +63,10 @@
                 Mais Games
             </p>
 
-            <div class="flex items-center justify-center gap-4 md:justify-start">
-                <a href="{{ route('gallows') }}" class="items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md inline-flex bg-[#02e659a5] hover:bg-[#02e659be] focus:bg-[#02e659a5] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800">Gallows</a>
-                <a href="{{ route('gallows.duo') }}" class="items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md inline-flex bg-[#02e659a5] hover:bg-[#02e659be] focus:bg-[#02e659a5] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800">DuoGallows</a>
-                <a href="{{ route('gallows.quadra') }}" class="items-center px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md inline-flex bg-[#02e659a5] hover:bg-[#02e659be] focus:bg-[#02e659a5] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800">QuadraGallows</a>
+            <div class="flex flex-col items-center justify-center w-full gap-4 xl:flex-row">
+                <a href="{{ route('gallows') }}" class="w-full px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md inline-flex bg-[#02e659a5] hover:bg-[#02e659be] focus:bg-[#02e659a5] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 justify-center items-center">Gallows</a>
+                <a href="{{ route('gallows.duo') }}" class="w-full px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md inline-flex bg-[#02e659a5] hover:bg-[#02e659be] focus:bg-[#02e659a5] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 justify-center items-center">DuoGallows</a>
+                <a href="{{ route('gallows.quadra') }}" class="w-full px-4 py-2 text-xs font-semibold tracking-widest text-white uppercase transition duration-150 ease-in-out border border-transparent rounded-md inline-flex bg-[#02e659a5] hover:bg-[#02e659be] focus:bg-[#02e659a5] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 justify-center items-center">QuadraGallows</a>
             </div>
         </div>
     </div>
