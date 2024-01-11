39d71847e9b796006235e56d22d2b647192b4f6b
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2024-01-11 20:43:53 -0300

    size box

--- resources/views/components/box.blade.php
+++ resources/views/components/box.blade.php
@@ -1,4 +1,4 @@
-<div class="flex items-center justify-center w-[26px] h-[26px] text-xl text-center text-white uppercase bg-black border border-[#181818] rounded-md 2xl:h-16 2xl:w-16">
+<div class="flex items-center justify-center text-xl text-center text-white uppercase bg-black border border-[#181818] rounded-md md:h-16 md:w-16 h-10 w-10">
     @if (in_array($w, $correctLetters))
         {{ $w }}
     @endif
