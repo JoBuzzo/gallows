5f2fcd5d8e20ba8a1cf84284cf1c4dc0366bb908
João Lucas Buzzo <108816555+JoBuzzo@users.noreply.github.com>
2024-01-17 09:11:39 -0300

    if (inputField)

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,29 +1,25 @@
 @if (!$modal)
-<input 
-    wire:keydown.enter="handleKeyDown"
-    wire:model="key"
-    class="uppercase bg-transparent border-0 focus:border-0 focus:ring-0 ring-0"
-    id="key"
-    placeholder="Digite uma letra"
-    autocomplete="off"
-/>
+    <input wire:keydown.enter="handleKeyDown" wire:model="key"
+        class="uppercase bg-transparent border-0 focus:border-0 focus:ring-0 ring-0" id="key"
+        placeholder="Digite uma letra" autocomplete="off" />
     <script>
         document.addEventListener("livewire:navigated", function() {
             const inputField = document.getElementById('key');
 
-            function maintainFocus() {
-                if (window.innerWidth > 768) {
-                    inputField.focus();
+            if (inputField) {
+                function maintainFocus() {
+                    if (window.innerWidth > 768) {
+                        inputField.focus();
+                    }
                 }
+                setInterval(maintainFocus, 1);
+
+                inputField.addEventListener('keydown', function(event) {
+                    if (event.key === 'Enter') {
+                        inputField.value = '';
+                    }
+                });
             }
-            setInterval(maintainFocus, 1);
-            inputField.addEventListener('keydown', function(event) {
-                if (event.key === 'Enter') {
-                    inputField.value = '';
-                }
-            });
         });
     </script>
 @endif
-
-
